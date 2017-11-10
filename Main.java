cda23c9d15bc21cf7ee1fd4af78fddd8a7a6d569
Monika Devanga Ravi <monikadr29@gmail.com>
2017-11-09 17:46:23 -0800

    video audio sync working

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main (String[] args) throws InterruptedException {
-		int nFrames=6000;
+		int nFrames = 6000;
 		int nBytes = 304128;
 		
 		int[] byteIndicies = new int[nFrames];
